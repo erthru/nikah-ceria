4f438c73aa22a60b1f6067d2ef39e6a7dfabc8d7
erthru <ersaka96@gmail.com>
2023-09-05 20:33:43 +0800

    Improvement invitation other

--- app/Http/Controllers/dashboard/invitations/detail/OtherController.php
+++ app/Http/Controllers/dashboard/invitations/detail/OtherController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\dashboard\invitations\detail;
 
 use App\Http\Controllers\Controller;
 use App\Models\Invitation;
+use App\Models\InvitationEvent;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
@@ -13,10 +14,12 @@ class OtherController extends Controller
     {
         $invitation = Invitation::with(['product', 'customer'])->findOrFail($id);
         $this->authorize('show-invitation', $invitation);
+        $invitationEvents = InvitationEvent::where('invitation_id', $id)->orderBy('id', 'asc')->get();
 
         return view('pages.dashboard.invitations.detail.other', [
             'title' => 'Detail Invitation',
             'invitation' => $invitation,
+            'invitationEvents' => $invitationEvents
         ]);
     }
 }
--- resources/views/pages/dashboard/invitations/detail/other.blade.php
+++ resources/views/pages/dashboard/invitations/detail/other.blade.php
@@ -21,14 +21,50 @@
                         <i class="bi bi-pencil-square"></i>
                         <span>Tambah</span>
                     </button>
-                    <table class="table table-striped nowrap w-100 ">
+                    <table id="eventTable" class="table table-striped nowrap w-100 ">
                         <thead>
                             <tr>
                                 <th scope="col">#</th>
                                 <th>Nama</th>
+                                <th>Tempat</th>
+                                <th>Alamat</th>
+                                <th>Koordinat</th>
+                                <th>Tgl & Waktu</th>
+                                <th>Perbarui</th>
+                                <th>Hapus</th>
                             </tr>
                         </thead>
                         <tbody>
+                            @foreach ($invitationEvents as $ie)
+                                <tr>
+                                    <td>{{ $loop->index + 1 }}</td>
+                                    <td>{{ $ie->name }}</td>
+                                    <td>{{ $ie->place }}</td>
+                                    <td>{{ $ie->address }}</td>
+                                    <td>
+                                        @if ($ie->latitude && $ie->longitude)
+                                            <a href="#" class="btn btn-danger text-white">
+                                                <i class="bi bi-geo-alt"></i>
+                                                <span>Buka Di Maps</span>
+                                            </a>
+                                        @else
+                                            Tidak tersedia
+                                        @endif
+                                    </td>
+                                    <td>
+                                        <button class="btn btn-danger text-white">
+                                            <i class="bi bi-pen"></i>
+                                            <span>Perbarui</span>
+                                        </button>
+                                    </td>
+                                    <td>
+                                        <button class="btn btn-danger text-white">
+                                            <i class="bi bi-trash"></i>
+                                            <span>Hapus</span>
+                                        </button>
+                                    </td>
+                                </tr>
+                            @endforeach
                         </tbody>
                     </table>
                 </div>
@@ -42,11 +78,14 @@
                         <i class="bi bi-pencil-square"></i>
                         <span>Tambah</span>
                     </button>
-                    <table class="table table-striped nowrap w-100 ">
+                    <table id="guestTable" class="table table-striped nowrap w-100 ">
                         <thead>
                             <tr>
                                 <th scope="col">#</th>
                                 <th>Nama</th>
+                                <th>Lihat Undangan</th>
+                                <th>Perbarui</th>
+                                <th>Hapus</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -63,11 +102,15 @@
                         <i class="bi bi-pencil-square"></i>
                         <span>Tambah</span>
                     </button>
-                    <table class="table table-striped nowrap w-100 ">
+                    <table id="giftTable" class="table table-striped nowrap w-100 ">
                         <thead>
                             <tr>
                                 <th scope="col">#</th>
-                                <th>Nama</th>
+                                <th>Bank</th>
+                                <th>Atas Nama</th>
+                                <th>No. Rekening</th>
+                                <th>Perbarui</th>
+                                <th>Hapus</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -96,7 +139,17 @@
 @section('script')
     <script>
         setTimeout(function() {
-            $('.table').DataTable({
+            $('#eventTable').DataTable({
+                lengthChange: false,
+                responsive: true
+            })
+
+            $('#guestTable').DataTable({
+                lengthChange: false,
+                responsive: true
+            })
+
+            $('#giftTable').DataTable({
                 lengthChange: false,
                 responsive: true
             })
