3841a2f7699c224d87e88722e432962485f65d50
erthru <ersaka96@gmail.com>
2023-09-08 15:13:05 +0800

    Improvement dashboard

--- app/Http/Controllers/dashboard/IndexController.php
+++ app/Http/Controllers/dashboard/IndexController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers\dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Models\Invitation;
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
@@ -11,8 +15,17 @@ class IndexController extends Controller
 {
     public function show(): View
     {
+        $usersTotal = User::count();
+        $productsTotal = Product::count();
+        $invitationsTotal = Invitation::count();
+        $ordersTotal = Order::count();
+
         return view('pages.dashboard.index', [
-            'title' => 'Dashboard'
+            'title' => 'Dashboard',
+            'usersTotal' => $usersTotal,
+            'productsTotal' => $productsTotal,
+            'invitationsTotal' => $invitationsTotal,
+            'ordersTotal' => $ordersTotal
         ]);
     }
 }
--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -3,9 +3,47 @@
 @section('content')
     <section class="py-1">
         <div class="mx-auto container">
-            <div class="card">
-                <div class="card-body">
-                    <p>it's dashboard</p>
+            <nav aria-label="breadcrumb" style="margin-top: -10px">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item active">Dashboard</li>
+                </ol>
+            </nav>
+            <div class="w-100 d-flex gap-3 flex-column flex-md-row">
+                <div class="card w-100 bg-primary text-white">
+                    <div class="card-body">
+                        <p class="fw-bold fs-4">{{ number_format($usersTotal, 0, ',', '.') }}</p>
+                        <div class="d-flex align-items-center column-gap-2 mt-2">
+                            <i class="bi bi-people fs-5"></i>
+                            <span>Pengguna</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="card w-100 bg-success text-white">
+                    <div class="card-body">
+                        <p class="fw-bold fs-4">{{ number_format($productsTotal, 0, ',', '.') }}</p>
+                        <div class="d-flex align-items-center column-gap-2 mt-2">
+                            <i class="bi bi-box fs-5"></i>
+                            <span>Produk</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="card w-100 bg-warning">
+                    <div class="card-body">
+                        <p class="fw-bold fs-4">{{ number_format($invitationsTotal, 0, ',', '.') }}</p>
+                        <div class="d-flex align-items-center column-gap-2 mt-2">
+                            <i class="bi bi-envelope-paper fs-5"></i>
+                            <span>Undangan</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="card w-100 bg-danger text-white">
+                    <div class="card-body">
+                        <p class="fw-bold fs-4">{{ number_format($ordersTotal, 0, ',', '.') }}</p>
+                        <div class="d-flex align-items-center column-gap-2 mt-2">
+                            <i class="bi bi-border-width fs-5"></i>
+                            <span>Orderan</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
